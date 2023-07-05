e55316bb9c601b326b8cb919306f8d8b2f4420bb
Zak Fixler <zakfixler@gmail.com>
2023-07-05 07:39:57 -0400

    Initial commit

---
+++ src/routes/register/+page.server.js
@@ -0,0 +1,56 @@
+import prisma from '$lib/prisma';
+import bcrpyt from 'bcrypt';
+import { redirect, json } from '@sveltejs/kit';
+
+/**
+ * @typedef { import("@prisma/client").User } User
+ * @typedef { import("@prisma/client").Prisma.UserCreateInput } UserCreateInput
+ */
+
+/**
+ * Add new user to the database
+ * @param {UserCreateInput} user
+ * @returns {Promise<User>}
+ */
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	default: async ({ request, cookies }) => {
+		const form = await request.formData();
+		const firstName = form.get('firstName');
+		const lastName = form.get('lastName');
+		const email = form.get('email');
+		const password = form.get('password');
+
+        /**
+         * Check to see if user already exists in the database.
+         * @param {string} email
+         * @returns {User|null}
+         */
+		const currentUser = await prisma.user.findUnique({
+			where: {
+				email: String(email).toLowerCase(),
+			},
+		});
+
+		if (currentUser) {
+			throw redirect(303, '/login');
+		}
+
+        const encryptedPassword = await bcrpyt.hash(String(password), 10);
+
+        /**
+         * Adds new user to the database.
+         * @param {UserCreateInput} user
+         * @returns {Promise<User>}
+         */
+        const newUser = await prisma.user.create({
+          data: {
+            firstName: String(firstName),
+            lastName: String(lastName),
+            email: String(email),
+            password: encryptedPassword
+          }
+        });
+	},
+};
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	let firstName = '';
+	let lastName = '';
+	let email = '';
+	let password = '';
+	let confirmPassword = '';
+
+	/**
+	 * Compare both passwords to verify that they match.
+	 * @param {string} password
+	 * @param {string} confirmPassword
+	 * @returns {boolean}
+	 */
+	function validatePassword(password, confirmPassword) {
+		if (!password || !confirmPassword) return false;
+		return password === confirmPassword;
+	}
+</script>
+
+<form method="post">
+	<label for="firstName">
+		First name
+		<input type="text" name="firstName" bind:value={firstName} required />
+	</label>
+	<label for="lastName">
+		Last name
+		<input type="text" name="lastName" bind:value={lastName} required />
+	</label>
+	<label for="email">
+		Email address
+		<input type="email" name="email" bind:value={email} required />
+	</label>
+	<label for="password">
+		Password
+		<input type="password" name="password" bind:value={password} required />
+	</label>
+	<label for="confirmPassword">
+		Confirm Password
+		<input type="confirmPassword" name="confirmPassword" bind:value={confirmPassword} required />
+	</label>
+	<button type="submit" disabled={!validatePassword(password, confirmPassword)}>Submit</button>
+</form>
